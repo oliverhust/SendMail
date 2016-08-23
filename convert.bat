d6164e109affa0b762339e10cd6c2e2035fdb26e
liangjinchao <liangjinchao@d3p.com>
2016-08-23 14:14:41 +0800

    添加背景和部分图标

--- convert.bat
+++ convert.bat
@@ -3,4 +3,7 @@ call pyuic4 -o ui_add_account.py ui_add_account.ui
 call pyuic4 -o ui_progress.py ui_progress.ui
 call pyuic4 -o ui_ndr.py ui_ndr.ui
 call pyuic4 -o ui_recv_host.py ui_recv_host.ui
+
+call pyrcc4 -o send1_rc.py send1.qrc
+
 :cmd
